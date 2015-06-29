8cac9da81dcc3d2193257733cede1050991e3cf2
Ben Wibking <wibking.1@osu.edu>
2015-06-29 00:31:11 -0400

    fixed shell script for convenience

---
+++ runspatial.xcorr.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+## compute spatial correlation function
+##  with TPACF code (Dolence & Brunner 2007)
+## b. wibking, jun 2015
+
+TPACF=$HOME/tpacf
+
+DOSPATIAL=1
+JACKKNIFE_SAMPLES=16
+
+DOAUTOCORR=0
+DOCROSSCORR=1
+
+NBINS=30
+MINBIN=0.1
+MAXBIN=30
+BOXSIZE=1911.2
+
+THREADS=16
+TEMPFILE=sfilelist.xcorr
+TEMPFILE2=sparams.xcorr
+
+echo "HOD_particles.hdf5 1
+subsample_particles.hdf5 1" > $TEMPFILE
+
+echo "$TEMPFILE
+$DOAUTOCORR
+$DOCROSSCORR
+0
+5
+$NBINS
+$MINBIN
+$MAXBIN
+1
+$BOXSIZE" > $TEMPFILE2
+
+echo "Converting HDF5 to unformatted binary"
+$TPACF/bin/precompute $TEMPFILE $DOSPATIAL $JACKKNIFE_SAMPLES
+
+echo "Computing spatial correlation"
+OMP_NUM_THREADS=$THREADS $TPACF/bin/correlate $TEMPFILE2
+
+
+
